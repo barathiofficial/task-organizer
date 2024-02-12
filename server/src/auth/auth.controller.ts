dd83dfa79eb82047671c86b3aec2bb94d4dee4ef
Barathi <barathiofficial@gmail.com>
2024-02-12 19:46:23 +0530

    AuthModule refactor

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,19 +1,74 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common'
+import {
+	BadRequestException,
+	Body,
+	ConflictException,
+	Controller,
+	HttpCode,
+	HttpStatus,
+	InternalServerErrorException,
+	Post
+} from '@nestjs/common'
+import { ApiTags } from '@nestjs/swagger'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
+import { UserService } from '../user/user.service'
 import { AuthService } from './auth.service'
 import { AuthDto } from './dto'
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
-	constructor(private readonly authService: AuthService) {}
+	constructor(
+		private readonly authService: AuthService,
+		private readonly userService: UserService
+	) {}
 
 	@Post('signup')
-	signup(@Body() data: AuthDto) {
-		return this.authService.signup(data)
+	async signup(@Body() data: AuthDto) {
+		try {
+			data.password = await this.authService.hashPassword(data.password)
+
+			const user = await this.userService.create(data)
+
+			delete user.password
+
+			return user
+		} catch (error) {
+			if (error instanceof PrismaClientKnownRequestError) {
+				if (error.code === 'P2002') {
+					throw new ConflictException('Email already exist')
+				}
+			}
+
+			throw new InternalServerErrorException(error)
+		}
 	}
 
 	@HttpCode(HttpStatus.OK)
 	@Post('login')
-	login(@Body() data: AuthDto) {
-		return this.authService.login(data)
+	async login(@Body() data: AuthDto) {
+		try {
+			const user = await this.userService.findOneByEmail(data.email)
+
+			if (!user) {
+				throw new BadRequestException('Wrong email/password')
+			}
+
+			const verified = await this.authService.verifyPassword(
+				user.password,
+				data.password
+			)
+
+			if (!verified) {
+				throw new BadRequestException('Wrong email/password')
+			}
+
+			delete user.password
+
+			const access_token = await this.authService.signToken(user.id)
+
+			return { access_token, user }
+		} catch (error) {
+			throw error
+		}
 	}
 }
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,74 +1,21 @@
-import {
-	BadRequestException,
-	ConflictException,
-	Injectable,
-	InternalServerErrorException
-} from '@nestjs/common'
+import { Injectable } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { JwtService } from '@nestjs/jwt'
-import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
 import * as argon from 'argon2'
-import { DatabaseService } from '../database/database.service'
-import { AuthDto } from './dto'
 
 @Injectable()
 export class AuthService {
 	constructor(
-		private db: DatabaseService,
 		private jwt: JwtService,
 		private config: ConfigService
 	) {}
 
-	async signup(data: AuthDto) {
-		try {
-			const password = await argon.hash(data.password, {})
-
-			data.password = password
-
-			const user = await this.db.user.create({
-				data
-			})
-
-			delete user.password
-
-			return user
-		} catch (error) {
-			if (error instanceof PrismaClientKnownRequestError) {
-				if (error.code === 'P2002') {
-					throw new ConflictException('Email already exist')
-				}
-			}
-
-			throw new InternalServerErrorException(error)
-		}
+	hashPassword(password: string) {
+		return argon.hash(password)
 	}
 
-	async login(data: AuthDto) {
-		try {
-			const user = await this.db.user.findUnique({
-				where: {
-					email: data.email
-				}
-			})
-
-			if (!user) {
-				throw new BadRequestException('Wrong email/password')
-			}
-
-			const verified = await argon.verify(user.password, data.password)
-
-			if (!verified) {
-				throw new BadRequestException('Wrong email/password')
-			}
-
-			delete user.password
-
-			const access_token = await this.signToken(user.id)
-
-			return { access_token }
-		} catch (error) {
-			throw error
-		}
+	verifyPassword(hash: string, password: string) {
+		return argon.verify(hash, password)
 	}
 
 	signToken(userId: string) {
@@ -77,7 +24,7 @@ export class AuthService {
 		}
 
 		return this.jwt.signAsync(payload, {
-			expiresIn: '1m',
+			expiresIn: '30d',
 			secret: this.config.get('JWT_SECRET')
 		})
 	}
