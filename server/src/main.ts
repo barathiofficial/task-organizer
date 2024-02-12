d8f0b305da212eaa20ab79e9eca1a90adcaf1408
Barathi <barathiofficial@gmail.com>
2024-02-12 16:12:39 +0530

    Add validation pipe

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,29 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { BadRequestException, ValidationPipe } from '@nestjs/common'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule)
+
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			transform: true,
+			stopAtFirstError: true,
+			exceptionFactory: (errors) => {
+				return new BadRequestException(
+					errors.map((error) => {
+						return {
+							field: error.property,
+							error: Object.values(error.constraints)[0]
+						}
+					})
+				)
+			}
+		})
+	)
+
+	await app.listen(3001)
 }
-bootstrap();
+
+bootstrap()
