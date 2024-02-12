489d120bd0892070e8756463339d23c8b5b346fd
Barathi <barathiofficial@gmail.com>
2024-02-12 19:47:24 +0530

    Swagger property

--- server/src/auth/dto/auth.dto.ts
+++ server/src/auth/dto/auth.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger'
 import { IsEmail, IsNotEmpty, IsStrongPassword } from 'class-validator'
 
 export class AuthDto {
+	@ApiProperty()
 	@IsEmail({}, { message: 'Invalid email' })
 	@IsNotEmpty({ message: 'Email is required' })
 	email: string
 
+	@ApiProperty()
 	@IsStrongPassword(
 		{
 			minLength: 8,
