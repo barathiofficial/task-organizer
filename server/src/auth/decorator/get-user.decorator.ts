30518114ba4ea737627e39ef0219d160cbe25fb8
Barathi <barathiofficial@gmail.com>
2024-02-12 19:47:05 +0530

    GetUser decorator refactor

--- server/src/auth/decorator/get-user.decorator.ts
+++ server/src/auth/decorator/get-user.decorator.ts
@@ -1,7 +1,12 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common'
 
 export const GetUser = createParamDecorator(
-	(data: unknown, ctx: ExecutionContext) => {
-		return ctx.switchToHttp().getRequest().user
+	(data: string | undefined, ctx: ExecutionContext) => {
+		const user = ctx.switchToHttp().getRequest().user
+		if (data && data in user) {
+			return user[data]
+		}
+
+		return user
 	}
 )
