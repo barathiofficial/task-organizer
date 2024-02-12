8e4c7abb1c6a1cfa96876001ab0dbd824883313f
Barathi <barathiofficial@gmail.com>
2024-02-12 16:42:48 +0530

    Add http decorator

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,14 +1,21 @@
-import { Controller, Get, UseGuards } from '@nestjs/common'
+import {
+	Controller,
+	Get,
+	HttpCode,
+	HttpStatus,
+	UseGuards
+} from '@nestjs/common'
 import { User } from '@prisma/client'
-import { UserService } from './user.service'
 import { GetUser } from '../auth/decorator'
 import { JwtGuard } from '../auth/guard'
+import { UserService } from './user.service'
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
 	constructor(private readonly userService: UserService) {}
 
+	@HttpCode(HttpStatus.OK)
 	@Get('me')
 	findOne(@GetUser() user: User) {
 		return user
