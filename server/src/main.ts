11f7a4bb4e6c58d633beb20a9f3daa947986f620
Barathi <barathiofficial@gmail.com>
2024-02-12 19:46:04 +0530

    Swagger init

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,6 @@
 import { BadRequestException, ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
@@ -22,6 +23,15 @@ async function bootstrap() {
 			}
 		})
 	)
+
+	const config = new DocumentBuilder()
+		.setTitle('Task Oragnizer API')
+		.setDescription('Task Oragnizer API')
+		.setVersion('1.0')
+		.build()
+
+	const document = SwaggerModule.createDocument(app, config)
+	SwaggerModule.setup('docs', app, document)
 
 	await app.listen(3001)
 }
