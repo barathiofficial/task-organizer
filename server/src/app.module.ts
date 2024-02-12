ab7eff73833ccbb39977abf713a531ddaeebdd2f
Barathi <barathiofficial@gmail.com>
2024-02-12 16:11:54 +0530

    Import required modules

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,19 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { validate } from 'class-validator'
+import { AuthModule } from './auth/auth.module'
+import { DatabaseModule } from './database/database.module'
+import { UserModule } from './user/user.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({
+			validate,
+			isGlobal: true
+		}),
+		DatabaseModule,
+		AuthModule,
+		UserModule
+	]
 })
 export class AppModule {}
