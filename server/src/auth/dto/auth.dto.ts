201d2d4e52445fb2a34746e076a47d9ffa54485d
Barathi <barathiofficial@gmail.com>
2024-02-14 00:07:24 +0530

    Refactor auth dto

--- server/src/auth/dto/auth.dto.ts
+++ server/src/auth/dto/auth.dto.ts
@@ -1,23 +1,7 @@
-import { ApiProperty } from '@nestjs/swagger'
-import { IsEmail, IsNotEmpty, IsStrongPassword } from 'class-validator'
+import { PickType } from '@nestjs/swagger'
+import { UserDto } from '../../user/dto'
 
-export class AuthDto {
-	@ApiProperty()
-	@IsEmail({}, { message: 'Invalid email' })
-	@IsNotEmpty({ message: 'Email is required' })
-	email: string
-
-	@ApiProperty()
-	@IsStrongPassword(
-		{
-			minLength: 8,
-			minLowercase: 2,
-			minNumbers: 2,
-			minSymbols: 2,
-			minUppercase: 2
-		},
-		{ message: 'Password is not strong enough' }
-	)
-	@IsNotEmpty({ message: 'Password is required' })
-	password: string
-}
+export class AuthDto extends PickType(UserDto, [
+	'email',
+	'password'
+] as const) {}
