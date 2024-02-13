671515ecf4bb5c0708d566aa26763c71ee568a14
Barathi <barathiofficial@gmail.com>
2024-02-14 00:08:11 +0530

    Refactor userdto

--- server/src/user/dto/user.dto.ts
+++ server/src/user/dto/user.dto.ts
@@ -1,15 +1,22 @@
-import { ApiProperty } from '@nestjs/swagger'
-import { IsNotEmpty, IsString, IsStrongPassword } from 'class-validator'
+import { ApiProperty, PickType } from '@nestjs/swagger'
+import { User } from '@prisma/client'
+import {
+	IsEmail,
+	IsNotEmpty,
+	IsString,
+	IsStrongPassword
+} from 'class-validator'
 
-export class UpdateUserDto {
-	@ApiProperty()
-	@IsString({ message: 'Name should be a string' })
-	@IsNotEmpty({ message: 'Name is required' })
-	name: string
-}
+export class UserDto implements User {
+	@ApiProperty({ example: 'ba7dd23c-618f-47e4-ad89-e696b71e8077' })
+	id: string
 
-export class UpdatePasswordDto {
-	@ApiProperty()
+	@ApiProperty({ example: 'johndoe@example.com' })
+	@IsEmail({}, { message: 'Invalid email' })
+	@IsNotEmpty({ message: 'Email is required' })
+	email: string
+
+	@ApiProperty({ example: 'JOhn!@12' })
 	@IsStrongPassword(
 		{
 			minLength: 8,
@@ -22,4 +29,21 @@ export class UpdatePasswordDto {
 	)
 	@IsNotEmpty({ message: 'Password is required' })
 	password: string
+
+	@ApiProperty({ example: 'John Doe' })
+	@IsString({ message: 'Name should be a string' })
+	@IsNotEmpty({ message: 'Name is required' })
+	name: string
+
+	@ApiProperty({ example: new Date() })
+	createdAt: Date
+
+	@ApiProperty({ example: new Date() })
+	updatedAt: Date
 }
+
+export class UpdateUserDto extends PickType(UserDto, ['name'] as const) {}
+
+export class UpdatePasswordDto extends PickType(UserDto, [
+	'password'
+] as const) {}
